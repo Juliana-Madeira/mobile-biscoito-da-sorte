4aca148d6ff39a7f6f1b1591dfaed6a396f56439
Juliana Madeira <juliwood1980@gmail.com>
2023-01-26 13:49:38 -0300

    create app biscoito da sorte

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [phrase, setPhrase] = useState('')
+  const [image, setImage] = useState(require('./assets/biscoito.png'))
+
+  const phrases = [
+    'Siga os bons e aprenda com eles.',
+    'O bom senso vale mais do que muito conhecimento.',
+    'O riso é a menor distância entre duas pessoas.',
+    'Deixe de lado as preocupações e seja feliz.',
+    'Realize o óbvio, pense no improvável e conquiste o impossível.',
+    'Acredite em milagres, mas não dependa deles.',
+    'A maior barreira para o sucesso é o medo do fracasso.'
+  ]
+
+  const handleCookie = () => {
+    let randomPhrase = phrases[Math.floor(Math.random() * phrases.length)]
+    console.log(randomPhrase)
+    setPhrase(randomPhrase)
+    setImage(require('./assets/biscoitoAberto.png'))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.img} source={image}/>
+      <Text style={styles.luckText}>"{phrase}"</Text>
+      <TouchableOpacity style={styles.button} onPress={handleCookie}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnText}>Quebrar Biscoito da Sorte</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +42,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: 250,
+    height: 250
+  },
+  luckText: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    textAlign: 'center',
+    fontStyle: 'italic'
+  },
+  button: {
+    width: 270,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#dd7b22',
+    fontWeight: '600'
+  }
 });
